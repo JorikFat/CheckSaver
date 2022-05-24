d7e42443e8f5d89b788a3ecb4359f0fcfc5c4fd1
balan <balanovkirill@gmail.com>
2022-05-24 15:36:57 +0300

    SharedPreferences

--- check/check-sample/src/main/java/dev/kirillbalanov/check_sample/SampleActivity.java
+++ check/check-sample/src/main/java/dev/kirillbalanov/check_sample/SampleActivity.java
@@ -1,28 +1,16 @@
 package dev.kirillbalanov.check_sample;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.TimePickerDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.text.InputType;
-import android.util.AttributeSet;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -43,11 +31,17 @@ public class SampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
 
-        init();
+        check = findViewById(R.id.tv_check);
+        //initialize preferences and set start latest saved date for check
+        pref = getSharedPreferences("Storage", MODE_PRIVATE);
+        check.setText(pref.getString(saveKey, "enter the data"));
 
-        myCustomDialog();
+        if(check.getText().toString().equals("enter the data")) {
+            myCustomDialog();
+        }
     }
 
+    @SuppressLint("SetTextI18n")
     private void myCustomDialog() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -60,73 +54,44 @@ public class SampleActivity extends AppCompatActivity {
 
         builder.setView(customLayout);
 
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                check.setText("Summary: " + number.getText() + " Date: " + date.getText() + " Time: " + time.getText());
-                SharedPreferences.Editor edit = pref.edit();
-                edit.putString(saveKey, check.getText().toString());
-                edit.apply();
-            }
+        builder.setPositiveButton("OK", (dialogInterface, i) -> {
+            check.setText("Summary: " + number.getText() + " Date: " + date.getText() + " Time: " + time.getText());
+            SharedPreferences.Editor edit = pref.edit();
+            edit.putString(saveKey, check.getText().toString());
+            edit.apply();
         });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                //only close
-            }
+        builder.setNegativeButton("Cancel", (dialogInterface, i) -> {
+            //only close
         });
         builder.show();
 
-        time.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showTimeDialog(time);
-            }
-        });
-        date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showDateDialog(date);
-            }
-        });
+        time.setOnClickListener(view -> showTimeDialog(time));
+        date.setOnClickListener(view -> showDateDialog(date));
     }
 
     private void showTimeDialog(TextView time) {
         calendar = Calendar.getInstance();
-        timeSetListener = new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-                calendar.set(Calendar.HOUR_OF_DAY, hour);
-                calendar.set(Calendar.MINUTE, minute);
+        timeSetListener = (timePicker, hour, minute) -> {
+            calendar.set(Calendar.HOUR_OF_DAY, hour);
+            calendar.set(Calendar.MINUTE, minute);
 
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+            @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
 
-                time.setText(simpleDateFormat.format(calendar.getTime()));
-            }
+            time.setText(simpleDateFormat.format(calendar.getTime()));
         };
         new TimePickerDialog(SampleActivity.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show();
     }
 
     private void showDateDialog(TextView date){
         calendar = Calendar.getInstance();
-        dateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                calendar.set(Calendar.YEAR, year);
-                calendar.set(Calendar.MONTH, month);
-                calendar.set(Calendar.DAY_OF_MONTH, day);
+        dateSetListener = (datePicker, year, month, day) -> {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.DAY_OF_MONTH, day);
 
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/mm/yyyy");
-                date.setText(simpleDateFormat.format(calendar.getTime()));
-            }
+            @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            date.setText(simpleDateFormat.format(calendar.getTime()));
         };
         new DatePickerDialog(SampleActivity.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
-    }
-
-    private void init(){
-        check = findViewById(R.id.tv_check);
-        //initialize preferences and set start latest saved date for check
-        pref = getSharedPreferences("Storage", MODE_PRIVATE);
-        check.setText(pref.getString(saveKey, "enter the data"));
     }
 }
