09d7a234314f5ee5ea4097e0b58045b2ff99dac3
JorikFat <jorikfat@gmail.com>
2022-05-31 01:37:31 +0300

    migrate to java

--- core/src/main/java/dev/jorik/checksaver/core/Utils.kt
+++
@@ -1,9 +0,0 @@
-package dev.jorik.checksaver.core
-
-import java.text.SimpleDateFormat
-import java.util.*
-
-val ruLocale = Locale("ru", "RU")
-
-val dateFormat = SimpleDateFormat("dd.MM.yyyy", ruLocale)
-val timeFormat = SimpleDateFormat("HH:mm", ruLocale)
