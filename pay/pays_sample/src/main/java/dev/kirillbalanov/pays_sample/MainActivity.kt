7b09e8dd87d6b85db33d1f58a353eb538dca6ff6
JorikFat <jorikfat@gmail.com>
2022-09-26 16:15:24 +0300

    create pays module

---
+++ pay/pays_sample/src/main/java/dev/kirillbalanov/pays_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.kirillbalanov.pays_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
